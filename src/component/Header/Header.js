13208e6e0cb2cf59077ab4539f1d6e8b73da1327
Shivam Goel <104217409+ShivamGoel3@users.noreply.github.com>
2023-07-18 22:00:05 +0530

    search

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -2,7 +2,7 @@ import './Header.css';
 
 const Header = () => {
     return (
-      <div className="headerss">
+      <div className="header">
       <span onClick={() => window.scroll(0, 0)} className="header">
          MOVIES AND TV SERIES 
       </span>
